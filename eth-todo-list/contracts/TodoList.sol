3c41013f323a309165ea2de1684de2ee8a410043
Mustafa Wahid <mustafarwahid@gmail.com>
2022-07-09 11:03:30 -0400

    - eth todo list updated

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -18,4 +18,19 @@ contract TodoList {
     //you want the public to see these tasks => public keyword.
     mapping(uint => Task) public tasks; 
 
+
+    /*function create*/
+    function createTask(string memory _content) public {
+        taskCount++; 
+        //tasks refers to the mapping. 
+        //using the taskCount as the id
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    //constructor function 
+
+    constructor() public {
+        createTask("Take out the trash.");
+    }
+
 }

8abb933076e5345efbf4b52f0bf99b2b52d6ddcd
Mustafa Wahid <mustafarwahid@gmail.com>
2022-07-12 07:11:50 -0400

    dapp deployed with fresh eth

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -7,13 +7,40 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
-    //constructor 
-    constructor() {
+    event NewWave(address indexed from, uint256 timestamp, string message); 
+
+    //custom wave datatype 
+    struct Wave {
+        address waver;
+        string message;
+        uint timestamp;
     }
 
-    function wave() public { //this function adds new waves to waveCounter
+    //array of structs 
+    Wave[] waves;
+
+    //constructor 
+    constructor() payable {
+        console.log("I am a smart contract Dogg!");
+    }
+
+    function wave(string memory _message) public { //this function adds new waves to waveCounter
         totalWaves +=1;
-        console.log("%s has waved", msg.sender);
+        console.log("%s has waved", msg.sender, _message);
+
+        //store the new message in the array 
+        waves.push(Wave(msg.sender,_message, block.timestamp));
+
+        //
+        emit NewWave(msg.sender, block.timestamp, _message);
+        uint256 prizeAmount = 0.0001 ether;
+        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than contract has.");
+        (bool success,) = (msg.sender).call{value:prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) { //this function gets all the waves 
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,36 +1,57 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy(
+        {
+            value: hre.ethers.utils.parseEther("0.1"),
+        } 
+    );
     await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+    console.log("Contract addy:", waveContract.address);
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
+  /*
+   * Get Contract balance to see what happened!
+   */
+    //contractBalance 
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:", 
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+    /**
+     * Let's send a few waves!
+     */
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-    waveCount = await waveContract.getTotalWaves();
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); // Wait for the transaction to be mined
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
+    //new contract balance 
+    console.log(
+        "Contract balance:", 
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
-    waveCount = await waveContract.getTotalWaves();
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
-
-
 
 const runMain = async () => {
     try {
         await main();
-        process.exit(0); // exit Node process without error
+        process.exit(0);
     } catch (error) {
         console.log(error);
-        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+        process.exit(1);
     }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
 };
 
 runMain();

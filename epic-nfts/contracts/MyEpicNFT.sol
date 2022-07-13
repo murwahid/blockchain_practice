c5c92cd85e5a3a9800c7be3c9013889e057926cc
Mustafa Wahid <mustafarwahid@gmail.com>
2022-07-13 11:48:43 -0400

    working MyEpicNFT.sol contract

---
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -0,0 +1,13 @@
+//license
+pragma solidity ^0.8.1;
+
+//imports 
+import "hardhat/console.sol";
+
+//contract declaration 
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is my NFT contract. Whoa!");
+    }
+
+}
